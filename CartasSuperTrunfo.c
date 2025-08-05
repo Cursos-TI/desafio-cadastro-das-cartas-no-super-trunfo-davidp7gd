82fc1957ffd818428edf687fab46734895d0e8ec
davidp7gd <davidpisno7@gmail.com>
2025-08-05 23:06:41 +0000

    adiçao dos super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,14 @@ int main() {
 char estado1[25];
 char codigo[50];
 char cidade[50];
-int populacao;
+int populaçao;
 float area;
 float pib;
 int turisticos;
 float dp;
 float pc;  
+float superpoder;
+
 printf("carta 1:\n");
 
 printf("digite o estado:\n");
@@ -23,7 +25,7 @@ printf("digite a cidade:\n");
 scanf("%s", cidade);
 
 printf("digite a populaçao:\n");
-scanf("%d", &populacao);
+scanf("%d", &populaçao);
 
 printf("digite area:\n");
 scanf("%f", &area);
@@ -34,30 +36,32 @@ scanf("%f", &pib);
 printf("digite o numeros de pontos turisticos\n");
 scanf("%d", &turisticos);
 
-dp = populacao / area;
-pc = (pib * 1000000000.0f) / populacao;
+dp = populaçao / area;
+pc = pib / populaçao;
+superpoder = populaçao + area + pib + turisticos + pc + dp;
 
 printf("carta 1:\n");
 printf("cidade: %s\n", cidade);
 printf("estado: %s\n", estado1);
 printf("codigo da carta: %s\n ", codigo);
-printf("populacao: %d\n area por km²: %.2f\n", populacao, area);
-printf("pib: %.2f\n pontos turisticos: %d\n", pib, turisticos);
-printf("densidade populacional: %.2f\n", dp);
-printf("pib per capita: %.2f\n", pc);
-
+printf("populaçao: %d\n area: %f\n", populaçao, area);
+printf("pib: %f\n pontos turisticos: %d\n", pib, turisticos);
+printf("dencidade populacional: %f\n", dp);
+printf("pib per capita: %f\n", pc);
+printf("super poder: %.2f\n", superpoder);
 
 
 
 char estado2[25];
 char codigo2[50];
 char cidade2[50];
-int populacao2;
+int populaçao2;
 float area2;
 float pib2;
 int turisticos2;
 float dp2;
 float pc2; 
+float superpoder2;
 
 printf("carta 2:\n");
 
@@ -71,7 +75,7 @@ printf("digite a cidade da carta2:\n");
 scanf("%s", cidade2);
 
 printf("digite a populaçao da carta2:\n");
-scanf("%d", &populacao2);
+scanf("%d", &populaçao2);
 
 printf("digite area da carta2:\n");
 scanf("%f", &area2);
@@ -82,9 +86,9 @@ scanf("%f", &pib2);
 printf("digite o numeros de pontos turisticos da carta2:\n");
 scanf("%d", &turisticos2);
 
-dp2 = populacao2 / area2;
-pc2 = (pib2 * 1000000000.0f) / populacao2;
-
+dp2 = populaçao2 / area2;
+pc2 = pib2 / populaçao2;
+superpoder2 = populaçao2 + area2 + pib2 + turisticos2 + pc2 + dp2;
 
 
 
@@ -92,11 +96,20 @@ printf("carta 2:\n");
 printf("cidade da cartaa2: %s\n", cidade2);
 printf("estado da carta2: %s\n", estado2);
 printf("codigo da carta2: %s\n ", codigo2);
-printf("populaçao da carta2: %d\n area em km² carta2: %.2f\n", populacao2, area2);
-printf("pib da carta2: %.2f\n pontos turisticos da carta2: %d\n", pib2, turisticos2);
-printf("densidade populacional carta2: %.2f\n", dp2);
-printf("pib per capita carta2: %.2f\n", pc2);
+printf("populaçao da carta2: %d\n area carta2: %f\n", populaçao2, area2);
+printf("pib da carta2: %f\n pontos turisticos da carta2: %d\n", pib2, turisticos2);
+printf("dencidade populacional carta2: %f\n", dp2);
+printf("pib per capita carta2: %f\n", pc2);
+printf("super poder: %.2f\n", superpoder2);
+
+printf("comparaçao das cartas:\n");
+printf("populaçao: %d\n", populaçao > populaçao2);
+printf("area: %d\n", area > area2);
+printf("pib: %d\n", pib > pib2);
+printf("pontos turisticoos: %d\n", turisticos > turisticos2);
+printf("densidade populacional: %d\n", dp < dp2);
+printf("pib oper capita: %d\n", pc > pc2);
+printf("super poder: %d\n", superpoder > superpoder2);
 
 return 0;
-
 }
